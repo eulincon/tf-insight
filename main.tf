1d49f72098fd61ff4b85b96e398c3fc7e102686b
lincon <linconsilva08@gmail.com>
2021-09-18 13:39:35 -0300

    v1.3

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     docker = {
-      source = "kreuzwerker/docker"
+      source  = "kreuzwerker/docker"
       version = "2.14.0"
     }
   }
@@ -9,4 +9,25 @@ terraform {
 
 provider "docker" {
 
+}
+
+resource "docker_image" "docusaurus-zup" {
+  name = "public.ecr.aws/zup-academy/docusaurus-zup:latest"
+}
+
+resource "docker_container" "docusaurus-zup" {
+  name  = "docusaurus-zup"
+  image = docker_image.docusaurus-zup.latest
+
+  ports {
+    internal = 3000
+  }
+}
+
+output "container_name" {
+  value = docker_container.docusaurus-zup.name
+}
+
+output "ip_address" {
+  value = join(":", [docker_container.docusaurus-zup.ip_address, docker_container.docusaurus-zup.ports[0].external])
 }
